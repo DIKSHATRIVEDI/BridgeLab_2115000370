0c70fda106eadfa0365ff6593bf6efbaa231eb08
Diksha Trivedi <diksha.trivedi2122@gmail.com>
2025-01-22 22:10:45 +0530

    Assignment 3 level 2 is uploaded

---
+++ Assign3_java_control_flow_level_2/AllMultipleOfNumber.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class AllMultipleOfNumber{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number ");
+        int number=sc.nextInt();
+        for(int i=100;i>=1;i--)if(i%number==0)System.out.println(i);
+    }
+}
